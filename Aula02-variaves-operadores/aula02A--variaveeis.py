63ce12333d7a02da3cbfb986f077730b64981277
jhonmeloteixeira@gmail.com <jhonmeloteixeira@gmail.com>
2026-03-16 20:45:40 -0300

    aula 2 de variavel

---
+++ Aula02-variaves-operadores/aula02A--variaveeis.py
@@ -0,0 +1,21 @@
+print("toma no cu")
+print(7+7)
+print("7-7")
+print("7" + "7") #Concatenado stirngs
+
+#comentario de uma linha
+
+#variaveis
+nome = "Melo"
+idade = 18
+peso = 80
+print(nome, idade, peso)
+print(f"vai se fude {nome}!")
+
+#imput= simulação de um forms no CMD
+
+nome = input("qual eh o seu nome prr: ")
+idade = input("digite sua idade:")
+peso = input("fale seu peso:")
+
+print(nome, idade, peso)
